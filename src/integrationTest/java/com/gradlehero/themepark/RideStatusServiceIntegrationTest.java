b4b71eeffff9c7f2ec82929f39f172bcc0f6dda6
Juan Manuel Romero <jmanuelromero93@gmail.com>
2022-04-03 14:36:46 -0500

    Project Init

---
+++ src/integrationTest/java/com/gradlehero/themepark/RideStatusServiceIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.gradlehero.themepark;
+
+
+import org.junit.jupiter.api.Test;
+
+public class RideStatusServiceIntegrationTest {
+
+    @Test
+    public void mainTest(){
+        RideStatusService.main(new String[]{"teacups"});
+    }
+}
